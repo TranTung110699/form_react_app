0d52329edf24f684902aec079cebdd92277c7e25
TranTung110699 <tranthanhtung110699@gmail.com.vn>
2024-04-21 15:14:36 +0700

    form-react-app commit ver 1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import RegisterPage from "./RegisterAccount/RegisterAccount";
+
+function App() {
+  return <RegisterPage />;
+}
+
+export default App;
